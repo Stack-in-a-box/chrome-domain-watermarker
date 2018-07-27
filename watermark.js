7821c47379edb2516c784a58c309a201ba6bf419
Andi Emma Davies <andidavies92@outlook.com>
2018-07-27 17:18:26 +0100

    Display translucent red overlay w/o text

--- watermark.js
+++ watermark.js
@@ -1,1 +1,14 @@
-document.body.style.backgroundColor = "blue";
+const overlay = document.createElement("div");
+
+overlay.setAttribute("style", `
+    z-index: 999999; /* This should be sufficiently high-enough to ensure it renders on-top of any properly-implemented website. */
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: red;
+    opacity: 0.25;
+`);
+
+document.body.appendChild(overlay);
