41bb7659273050d3d34899aef77a7b079f9cda77
Andi Emma Davies <andidavies92@outlook.com>
2018-08-01 19:19:45 +0100

    Remove the need for a paragraph element

--- watermark.js
+++ watermark.js
@@ -1,17 +1,13 @@
 attachStylesToDom();
 
 const overlay = document.createElement("div");
-const text = document.createElement("p");
-
 const textToRepeat = "WARNING: Live system!!"
 const spacingCharacters = " _____ ";
 const timesToRepeatText = 100;
 const repeatedText = (textToRepeat + spacingCharacters).repeat(timesToRepeatText);
 
-text.innerHTML = repeatedText; // TODO: Make sure "textToRepeat" gets sanitised, to prevent potential script injection attacks, once it's possible for users to specify their own watermark text.
-
 overlay.setAttribute("id", "overlay"); // TODO: Change this to something more-specific, so it's less likely to class with an app also using this ID for another purpose.
-overlay.appendChild(text);
+overlay.dataset.text = repeatedText;
 
 document.body.appendChild(overlay);
 
