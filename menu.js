a401e3af59e8ab30c35d1982a0a4589b42ae3e34
Andi Emma Davies <andidavies92@outlook.com>
2018-08-19 18:12:02 +0100

    Add the ability to add domains to watermark list

--- menu.js
+++ menu.js
@@ -1,8 +1,26 @@
-chrome.storage.local.get({ domains: [] }, ({ domains }) => {
-    document.getElementById("domain-list").innerHTML = domains.map(domain => `
-        <li>
-            <span>${domain}</span>
-            <a href="#" class="remove-link">🗙</a>
-        </li>
-    `).join("");
+chrome.storage.local.get({ domains: [] }, storage => {
+    let domains = storage.domains;
+
+    (function main() {
+        document.getElementById("add-domain-form").addEventListener("submit", onFormSubmit);
+        renderDomainList();
+    })();
+
+    function renderDomainList() {
+        document.getElementById("domain-list").innerHTML = domains.map(domain => `
+            <li>
+                <span>${domain}</span>
+                <a href="#" class="remove-link">🗙</a>
+            </li>
+        `).join("");
+    }
+
+    // TODO: Sanitise input.
+    function onFormSubmit() {
+        const domainToAdd = document.getElementById("domain-to-add-field").value;
+        if (!domainToAdd) return;
+        domains.push(domainToAdd);
+        chrome.storage.local.set({ domains });
+        renderDomainList();
+    }
 });
